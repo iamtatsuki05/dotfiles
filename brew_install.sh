9df8d837369e791fe4c24f69128c535819ce279e
iamtatsuki05 <tatsukio0522@gmail.com>
2024-12-27 01:32:16 +0900

    update

--- brew_install.sh
+++ brew_install.sh
@@ -6,39 +6,48 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install cask
 
 # Install Applications
-brew install --cask clipy
 brew install --cask google-japanese-ime
 brew install --cask filezilla
 brew install --cask zoomus
+brew install --cask notion
+brew install --cask obsidian
+brew install --cask discord
+brew install --cask slack
 
 brew install --cask google-chrome
 brew install --cask firefox
+brew install --cask brave
 
 brew install --cask docker
+brew install --cask orbstack
 brew install --cask virtualbox
 
-brew install --cask iterm2
+brew install --cask vnc-viewer
 brew install --cask sourcetree
 brew install --cask visual-studio-code
+brew install --cask cursor
+brew install --cask android-studio
 brew install --cask coteditor
 brew install --cask sequel-pro
-brew install --cask brave
-brew install --cask warp
+brew install --cask clipy
+brew install --cask alacritty
 brew install --cask google-drive
 brew install --cask adobe-acrobat-reader
 brew install --cask alfred
-brew install --cask android-studio
 brew install --cask raycast
 brew install --cask unity-hub
-brew install --cask slack
+brew install --cask rectangle-pro
+brew install --cask betterdisplay
+brew install --cask wireshark
+brew install --cask obs
+
 
 # Install CLI tools
 brew install neovim
 brew install tmux
 brew install z
-brew install anyenv
-anyenv install --init
-echo 'eval "$(anyenv init -)"' >> ~/.zshrc
+brew install mise
+echo 'eval "$(/opt/homebrew/bin/mise activate zsh)"' >> ~/.zshrc
 brew install tree
 brew install git-lfs
 brew install ffmpeg
@@ -48,3 +57,7 @@ brew install gh
 brew install fzf
 brew install bat
 brew install ripgrep
+brew install derailed/k9s/k9s
+brew install cloudflared
+brew install uv
+brew install jq
