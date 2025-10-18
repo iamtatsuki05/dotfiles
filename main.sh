1813a3bcfa35d66cbf271ea891e7ee62c6624c9f
iamtatsuki05 <tatsukio0522@gmail.com>
2025-10-18 23:39:09 +0900

    Add Alacritty configuration and update main script for setup

--- main.sh
+++ main.sh
@@ -8,6 +8,8 @@ sh brew_install.sh
 
 # Setup default settings
 sh default_setup.sh
+mkdir -p ~/.config/alacritty
+cp configs/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 # Install neovimconfig
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -19,17 +21,17 @@ ln -s ~/.config/nvim/bundle/vim-code-dark.git/colors/codedark.vim ~/.config/nvim
 git clone https://github.com/iamtatsuki05/neovim_config.git
 cp neovim_config/init.vim ~/.config/nvim/
 
-echo """
-plesae run this command:
-# neovim
-:PlugInstall
+# Install Vim plugins automatically
+nvim -c 'PlugInstall' -c 'qa'
 
-# optional
-## terminal
-git clone https://github.com/github/copilot.vim \
+echo """
+Optional: Install GitHub Copilot
+Run the following commands:
+# terminal
+git clone https://github.com/github/copilot.vim \\
    ~/.config/nvim/pack/github/start/copilot.vim
-## neovim
-:Copilot setup
+# neovim
+nvim -c 'Copilot setup'
 """
 
 echo "Please restart your terminal."
