ce5874c81d6e5af16f271d710cc8bc3c85253d62
iamtatsuki05 <tatsukio0522@gmail.com>
2026-06-06 08:25:44 +0900

    feat: add google-colab-cli skill and evaluation suites

--- tests/test_dotfiles_test_runner.sh
+++ tests/test_dotfiles_test_runner.sh
@@ -205,6 +205,7 @@ test_mise_tasks_include_nix_migration_flow() {
   assert_contains "$MISE_CONFIG" 'https://downloads.cursor.com/lab/{{ version }}/{{ os(macos="darwin", linux="linux") }}/{{ arch(x64="x64", arm64="arm64") }}/agent-cli-package.tar.gz'
   assert_contains "$MISE_CONFIG" 'opencode = "latest"'
   assert_contains "$MISE_CONFIG" '"pipx:markitdown" = "latest"'
+  assert_contains "$MISE_CONFIG" '"pipx:google-colab-cli" = "latest"'
   assert_contains "$MISE_CONFIG" '"pipx:git+https://github.com/NousResearch/hermes-agent.git" = "latest"'
   assert_contains "$MISE_CONFIG" '"npm:@github/copilot" = "latest"'
   assert_contains "$MISE_CONFIG" '"npm:openclaw" = "latest"'
